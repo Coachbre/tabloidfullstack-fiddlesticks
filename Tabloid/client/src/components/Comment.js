7427360d75449468a7fe39810b65abadcb94f0cc
Laurel Morrison <lbentleym1992@gmail.com>
2021-07-14 13:00:52 -0400

    adding comment files

---
+++ Tabloid/client/src/components/Comment.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+export default function Comment({ comment }) {
+  return (
+    <Card className="m-4">
+      <CardBody>
+          <strong>{comment.subject}</strong>
+          <p>{comment.content}</p>
+          <p>{comment.UserProfile.displayName}</p>          
+          <p>{comment.creationDate}</p>
+      </CardBody>
+    </Card>
+  );
+}
