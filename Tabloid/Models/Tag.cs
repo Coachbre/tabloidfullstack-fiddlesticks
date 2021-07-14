28a33ca06dc189ebba81171df078b02244c8a9ae
Coachbre <brekonan1@gmail.com>
2021-07-14 12:54:34 -0400

    creating tag methods

---
+++ Tabloid/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tabloid.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
