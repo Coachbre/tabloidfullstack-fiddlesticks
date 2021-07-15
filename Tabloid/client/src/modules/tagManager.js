9cf6873b19ec320f2188f2ea88a8d4b23e7b3e37
Coachbre <brekonan1@gmail.com>
2021-07-15 13:02:01 -0400

    started working on tag deletion

--- Tabloid/client/src/modules/tagManager.js
+++ Tabloid/client/src/modules/tagManager.js
@@ -6,7 +6,21 @@ export const getAllTags = () => {
     .then((res) => res.json())
 };
 
+export const getTag = (id) => {
+    return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+};
+
 export const addTag = (tag) => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tag),
+    });
+};
+
+export const deleteTag = (tag) => {
     return fetch(baseUrl, {
         method: "POST",
         headers: {
