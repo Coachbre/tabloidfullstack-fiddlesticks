dcb81269d5281424581b32317d94a86f44bc98a4
Coachbre <brekonan1@gmail.com>
2021-07-16 15:20:20 -0400

    header changes

--- Tabloid/client/src/components/Header.js
+++ Tabloid/client/src/components/Header.js
@@ -43,66 +43,67 @@ export default function Header({ isLoggedIn }) {
 
                 <NavItem>
                   <NavLink tag={RRNavLink} to="/category">Category Management</NavLink>
-            {/* When isLoggedIn === true, we will render the Home link */}
-            {isLoggedIn && (
-              <>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/">
-                    Home
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink tag={RRNavLink} to="/post">Posts</NavLink>
-                </NavItem>
-                <NavItem>
+                  </>
+                {/* When isLoggedIn === true, we will render the Home link */}
+                {isLoggedIn && (
+                  <>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/">
+                        Home
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/post">Posts</NavLink>
+                    </NavItem>
+                    <NavItem>
 
-                <NavLink tag={RRNavLink} className="nav-link" to="/myPost">My Posts</NavLink>      
+                      <NavLink tag={RRNavLink} className="nav-link" to="/myPost">My Posts</NavLink>
 
-              </NavItem>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/tags">
-                    Tag Management
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/category">
-                    Categories
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/users">
-                    User Profiles
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <a
-                    aria-current="page"
-                    className="nav-link"
-                    style={{ cursor: "pointer" }}
-                    onClick={logout}
-                  >
-                    Logout
-                  </a>
-                </NavItem>
-              </>
-            )}
-            {!isLoggedIn && (
-              <>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/login">
-                    Login
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={RRNavLink} to="/register">
-                    Register
-                  </NavLink>
-                </NavItem>
-              </>
-            )}
-          </Nav>
-        </Collapse>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/tags">
+                        Tag Management
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/category">
+                        Categories
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/users">
+                        User Profiles
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <a
+                        aria-current="page"
+                        className="nav-link"
+                        style={{ cursor: "pointer" }}
+                        onClick={logout}
+                      >
+                        Logout
+                      </a>
+                    </NavItem>
+                  </>
+                )}
+                {!isLoggedIn && (
+                  <>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/login">
+                        Login
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={RRNavLink} to="/register">
+                        Register
+                      </NavLink>
+                    </NavItem>
+                  </>
+                )}
+              </Nav>
+              </Collapse>
       </Navbar>
-    </div>
-  );
+  </div>
+      );
 }
