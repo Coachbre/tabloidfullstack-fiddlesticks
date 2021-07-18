1342704df1c010bc3ee8b87e3ab76c74fcc2f7ed
Chadwell Clark <wcclark23@comcast.net>
2021-07-17 21:31:58 -0500

    New AuthManager thanks to Andy due to bug I found that was causing my isAdmin code to crash when registering because firebase was authenticating then setting isLoggedin before saving the new user

--- Tabloid/client/src/modules/authManager.js
+++ Tabloid/client/src/modules/authManager.js
@@ -1,9 +1,75 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
+export const getToken = () => {
+  const currentUser = firebase.auth().currentUser;
+  if (!currentUser) {
+    throw new Error("Cannot get current user. Did you forget to login?");
+  }
+  return currentUser.getIdToken();
+};
+
+export const login = (email, pw) => {
+  return firebase
+    .auth()
+    .signInWithEmailAndPassword(email, pw)
+    .then((signInResponse) => {
+      loginStatus.apply(AUTH_ACTIONS.FIREBASE_LOGIN);
+      return _doesUserExist(signInResponse.user.uid);
+    })
+    .then((doesUserExist) => {
+      if (doesUserExist) {
+        loginStatus.apply(AUTH_ACTIONS.API_LOGIN);
+      } else {
+        return logout().then(() => {
+          throw new Error(
+            "Something's wrong. The user exists in firebase, but not in the application database."
+          );
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
+};
+
+export const logout = () => {
+  return firebase
+    .auth()
+    .signOut()
+    .then(() => loginStatus.apply(AUTH_ACTIONS.LOGOUT));
+};
+
+export const register = (userProfile, password) => {
+  return firebase
+    .auth()
+    .createUserWithEmailAndPassword(userProfile.email, password)
+    .then((createResponse) => {
+      loginStatus.apply(AUTH_ACTIONS.FIREBASE_LOGIN);
+      return _saveUser({
+        ...userProfile,
+        firebaseUserId: createResponse.user.uid,
+      });
+    })
+    .then(() => loginStatus.apply(AUTH_ACTIONS.API_LOGIN));
+};
+
+export function onLoginStatusChange(handler) {
+  loginStatus.setOnLoginStatusChangedHandler(handler);
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      loginStatus.apply(AUTH_ACTIONS.REINSTATE_FIREBASE_LOGIN);
+    } else {
+      loginStatus.apply(AUTH_ACTIONS.LOGOUT);
+    }
+  });
+}
+
 const _apiUrl = "/api/userprofile";
 
-const _doesUserExist = (firebaseUserId) => {
+function _doesUserExist(firebaseUserId) {
   return getToken().then((token) =>
     fetch(`${_apiUrl}/DoesUserExist/${firebaseUserId}`, {
       method: "GET",
@@ -12,9 +78,9 @@ const _doesUserExist = (firebaseUserId) => {
       },
     }).then((resp) => resp.ok)
   );
-};
+}
 
-const _saveUser = (userProfile) => {
+function _saveUser(userProfile) {
   return getToken().then((token) =>
     fetch(_apiUrl, {
       method: "POST",
@@ -25,49 +91,75 @@ const _saveUser = (userProfile) => {
       body: JSON.stringify(userProfile),
     }).then((resp) => resp.json())
   );
+}
+
+const loginStatus = {
+  _state: INITIAL,
+  get _isLoggedIn() {
+    return this._state === INITIAL ? null : this._state === AUTHENTICATED;
+  },
+  apply(action) {
+    const prevIsLoggedIn = this._isLoggedIn;
+    this._state = this._state(action);
+    this._loginStatusChanged(prevIsLoggedIn);
+  },
+  setOnLoginStatusChangedHandler(handler) {
+    handler(this._isLoggedIn);
+    this._onLoginStatusChangedHandler = handler;
+  },
+  _onLoginStatusChangedHandler() {},
+  _loginStatusChanged(prevIsLoggedIn) {
+    if (prevIsLoggedIn !== this._isLoggedIn) {
+      this._onLoginStatusChangedHandler(this._isLoggedIn);
+    }
+  },
 };
 
-export const getToken = () => firebase.auth().currentUser?.getIdToken();
+const AUTH_ACTIONS = [
+  "REINSTATE_FIREBASE_LOGIN",
+  "FIREBASE_LOGIN",
+  "API_LOGIN",
+  "LOGOUT",
+].reduce((actions, action) => ({ ...actions, [action]: action }), {});
 
-export const login = (email, pw) => {
-  return firebase
-    .auth()
-    .signInWithEmailAndPassword(email, pw)
-    .then((signInResponse) => _doesUserExist(signInResponse.user.uid))
-    .then((doesUserExist) => {
-      if (!doesUserExist) {
-        // If we couldn't find the user in our app's database, we should logout of firebase
-        logout();
+function INITIAL(action) {
+  switch (action) {
+    case AUTH_ACTIONS.REINSTATE_FIREBASE_LOGIN:
+      return AUTHENTICATED;
+    case AUTH_ACTIONS.FIREBASE_LOGIN:
+      return FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED;
+    case AUTH_ACTIONS.LOGOUT:
+      return UNAUTHENTICATED;
+    default:
+      return INITIAL;
+  }
+}
 
-        throw new Error(
-          "Something's wrong. The user exists in firebase, but not in the application database."
-        );
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      throw err;
-    });
-};
+function UNAUTHENTICATED(action) {
+  switch (action) {
+    case AUTH_ACTIONS.FIREBASE_LOGIN:
+      return FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED;
+    default:
+      return UNAUTHENTICATED;
+  }
+}
 
-export const logout = () => {
-  firebase.auth().signOut();
-};
+function FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED(action) {
+  switch (action) {
+    case AUTH_ACTIONS.API_LOGIN:
+      return AUTHENTICATED;
+    case AUTH_ACTIONS.LOGOUT:
+      return UNAUTHENTICATED;
+    default:
+      return FIREBASE_AUTHENTICATED_API_UNAUTHENTICATED;
+  }
+}
 
-export const register = (userProfile, password) => {
-  return firebase
-    .auth()
-    .createUserWithEmailAndPassword(userProfile.email, password)
-    .then((createResponse) =>
-      _saveUser({
-        ...userProfile,
-        firebaseUserId: createResponse.user.uid,
-      })
-    );
-};
-
-export const onLoginStatusChange = (onLoginStatusChangeHandler) => {
-  firebase.auth().onAuthStateChanged((user) => {
-    onLoginStatusChangeHandler(!!user);
-  });
-};
+function AUTHENTICATED(action) {
+  switch (action) {
+    case AUTH_ACTIONS.LOGOUT:
+      return UNAUTHENTICATED;
+    default:
+      return AUTHENTICATED;
+  }
+}
