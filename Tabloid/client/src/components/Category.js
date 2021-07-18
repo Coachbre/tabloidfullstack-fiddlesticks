6eb438fd378abd0918780efa8c995e87395fbd61
NSPatton <greengun15@gmail.com>
2021-07-18 11:42:33 -0500

    delete functional, but no automatic refresh on delete. Remember to ask an instructor about it.

--- Tabloid/client/src/components/Category.js
+++ Tabloid/client/src/components/Category.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router";
+import { Button } from "reactstrap";
 import { Card, CardBody } from "reactstrap";
-import { Link } from "react-router-dom";
+import { deleteCategory } from "../modules/categoryManager";
 
 const Category = ({ category }) => {
+
+    const deleteACategory = (event) => {
+        event.preventDefault()
+        const confirmDelete = window.confirm("Are you sure you want to delete this category?")
+        if (confirmDelete) {
+            deleteCategory(category.id)
+        }
+    }
+
+
+
     return (
         <Card>
             <CardBody>
                 <p key={category.id}>{category.name}</p>
+                <Button color="danger" onClick={deleteACategory}>Delete</Button>
             </CardBody>
         </Card>
     )
