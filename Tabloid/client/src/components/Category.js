77d340178680bb8daa727c95ec68c3d9969c1bf8
NSPatton <greengun15@gmail.com>
2021-07-19 10:05:40 -0500

    used props to refresh page on delete

--- Tabloid/client/src/components/Category.js
+++ Tabloid/client/src/components/Category.js
@@ -1,18 +1,21 @@
-import React from "react";
-import { useHistory } from "react-router";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Button } from "reactstrap";
 import { Card, CardBody } from "reactstrap";
 import { deleteCategory } from "../modules/categoryManager";
 
-const Category = ({ category }) => {
+
+const Category = ({ category, getCategories }) => {
 
     const deleteACategory = (event) => {
         event.preventDefault()
         const confirmDelete = window.confirm("Are you sure you want to delete this category?")
         if (confirmDelete) {
-            deleteCategory(category.id)
+            deleteCategory(category.id).then(() => { getCategories() })
         }
     }
+
+
 
 
 
